ee004177204319d06c2e940f34c0dfa996e6504c
rclinton <ray.clinton@hughes.com>
2018-10-29 16:34:53 -0400

    Initial work

---
+++ classes/tags.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+class tags(object):
+    def __init__(self):
+        self.script_term = '===='
+        self.title = 'title'
+        self.credit = 'credit'
+        self.author = 'author'
+        self.source = 'source'
+        self.notes = 'notes'
+        self.copyright = 'copyright'
+        self.header = 'H'
+        self.page_break = 'b'
+        self.page = 'P'
+        self.page_all = 'PALL'
+        self.panel = 'p'
+        self.character = 'C'
+        self.note = 'n'
